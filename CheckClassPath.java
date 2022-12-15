98be020b98c5b2946aaf209bd89732faf30c77f3
hungvo2010 <hungvochanhdp1@gmail.com>
2022-12-15 16:54:37 +0700

    class path'

---
+++ CheckClassPath.java
@@ -0,0 +1,5 @@
+class CheckClassPath{
+    public static void main(String args[]){
+        System.out.println(System.getProperty("java.class.path"));
+    }
+}
---
+++ MoreComparatorTest.java
@@ -0,0 +1,8 @@
+import p1.Employee;
+
+public class MoreComparatorTest {
+    public static void main(String[] args) {
+        Employee[] emps = new Employee[5];
+        
+    }
+}
